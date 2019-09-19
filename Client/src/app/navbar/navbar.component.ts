4edf094449cd4867e16bdb8c58c5e78e8209f39d
robin <robinleesuns13@gmail.com>
2019-09-18 23:06:52 -0700

    feat: add logout method for navbar

--- Client/src/app/navbar/navbar.component.ts
+++ Client/src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '@app/_services';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authenticationService: AuthenticationService,
+    private router: Router,
+    ) { }
 
   ngOnInit() {
   }
+
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
+}
 
 }
