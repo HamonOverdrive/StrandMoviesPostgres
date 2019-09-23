0ccd839119b6e9a7a665f3c2afa4e88ac53dc851
robin <robinleesuns13@gmail.com>
2019-09-22 21:10:11 -0700

    feat: create movie model on client side

---
+++ Client/src/app/_models/movie.ts
@@ -0,0 +1,13 @@
+export class MovieList {
+  id: number;
+  title:  string;
+  year: string;
+  rated: string;
+  released: string;
+  genre: string;
+  director: string;
+  writer: string;
+  plot: string;
+  poster: string;
+  metascore: string;
+}
