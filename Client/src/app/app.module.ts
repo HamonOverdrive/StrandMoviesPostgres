d43883bd920b340171ea8b5e23a23379bd4eba66
robin <robinleesuns13@gmail.com>
2019-09-24 00:18:56 -0700

    feat: add formmodule for bootstrap and adjust navbar items to right and create search bar

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -29,6 +29,7 @@ import { StrandDetailsComponent } from './strand-details/strand-details.componen
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
+    FormsModule,
     HttpClientModule,
   ],
   providers: [
