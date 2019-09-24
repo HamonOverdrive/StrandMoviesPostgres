92eb9d9d313feee1ef495ffd13f202e643996204
robin <robinleesuns13@gmail.com>
2019-09-23 20:09:24 -0700

    feat: add back button to all lists and add if exists movies of the list html elements using angular

--- Client/src/app/strand-details/strand-details.component.ts
+++ Client/src/app/strand-details/strand-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AlertService, UserService, AuthenticationService, MovieListService } from '../_services';
+import { MovieList, User, Movie } from '@app/_models';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-strand-details',
@@ -8,14 +10,30 @@ import { AlertService, UserService, AuthenticationService, MovieListService } fr
   styleUrls: ['./strand-details.component.css']
 })
 export class StrandDetailsComponent implements OnInit {
-
+  strand : MovieList;
+  movies : Movie[];
   constructor(
     private route: ActivatedRoute,
     private movieListService: MovieListService,
-    private alertService: AlertService
+    private alertService: AlertService,
+    private location: Location
   ) { }
 
   ngOnInit() {
+    this.getById()
+  }
+
+  getById(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.movieListService.getById(id)
+      .subscribe(strand =>{
+        console.log(strand)
+        this.strand = strand
+      });
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
