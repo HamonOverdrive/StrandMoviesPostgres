9431bf26fb08b116661678cf0dd6690f38df73c8
robin <robinleesuns13@gmail.com>
2019-09-26 22:40:44 -0700

    feat: on movie detail page make wishlist dropdown basic styling

--- Client/src/app/movie-detail/movie-detail.component.ts
+++ Client/src/app/movie-detail/movie-detail.component.ts
@@ -1,21 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
 import {Router, ActivatedRoute, Params} from '@angular/router';
-import { AuthenticationService, AlertService, OMDBService } from '@app/_services';
+import { AuthenticationService, AlertService, OMDBService, MovieListService } from '@app/_services';
 import { MovieList, User, Movie } from '@app/_models';
+import { first } from 'rxjs/operators';
+
 @Component({
   selector: 'app-movie-detail',
   templateUrl: './movie-detail.component.html',
   styleUrls: ['./movie-detail.component.css']
 })
 export class MovieDetailComponent implements OnInit {
+  movieLists: MovieList[];
   movie : Movie;
+  currentUser: User;
+
   constructor(
       private omdbService: OMDBService,
-      private route: ActivatedRoute) { }
+      private route: ActivatedRoute,
+      private movieListService: MovieListService) { }
 
   ngOnInit() {
-    this.getByImdb()
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    this.getByImdb();
+    this.movieListService.getAllByUserId(this.currentUser.id).pipe(first()).subscribe(movieLists => {
+      console.log(movieLists)
+      this.movieLists = movieLists;
+    });
   }
 
   getByImdb(): void {
